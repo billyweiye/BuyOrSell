aecb06de270d5b32c930ade2f71a4af8ae87f4fc
billyweiye <wyeah1980@gmail.com>
2024-11-01 23:02:04 +0800

    Update trend_analysis.py

--- trend_analysis.py
+++ trend_analysis.py
@@ -1,4 +1,5 @@
 # analyze the trend of the stock with the indicators from indicators.py
+import numpy as np
 from indicators import calculate_trend_indicators
 import pandas as pd
 
@@ -9,6 +10,14 @@ def analyze_trend(data):
 
     # 计算趋势信号的累计和
     data['trend_signal'] = data[trend_signals].sum(axis=1)
+
+    # 给出买卖强烈的型号，若有超过80%的信号为趋势向上，则强烈买入，若超过60%的信号为趋势向上，则买入，若超过80%的信号为趋势向下，则强烈卖出，若超过60%的信号为趋势向下，则卖出, 其他情况为持有
+    conditions = [(data['trend_signal'] > len(trend_signals)*0.8),
+                 (data['trend_signal'] > len(trend_signals)*0.6),
+                 (data['trend_signal'] < -len(trend_signals)*0.8),
+                 (data['trend_signal'] < -len(trend_signals)*0.6)]
+    choices = ['strong_buy','buy','strong_sell','sell']
+    data['trend_signal_strong'] = np.select(conditions, choices, default='hold')
 
     return data
 
