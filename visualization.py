35f0f982b5f9510f79655148688853c0ebb4abe8
billyweiye <wyeah1980@gmail.com>
2024-11-03 20:34:51 +0800

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,15 +1,19 @@
 # visualization of the trend analysis
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt 
 import seaborn as sns
 
 def plot_trend_analysis(data):
     # set up the figure
-    fig, ax = plt.subplots(figsize=(10, 6)) 
+    fig, ax = plt.subplots(1) 
+    fig.set_figheight(15)
+    fig.set_figwidth(45)
 
     # plot the price first
     sns.lineplot(x='date', y='close', data=data, ax=ax) 
 
     # plot the buy_or_sell spot
-    sns.scatterplot(x='date',y='buy_or_sell',data=data,ax=ax)
+    custom_palette = {'buy': '#A5014D', 'strong_buy': '#710022','sell':'#848E2F','strong_sell':'#324103','hold':'blue'}
 
-    plt.show()
+    sns.scatterplot(x='date',y='close',hue='buy_or_sell',data=data,palette=custom_palette,ax=ax)
+
+    return fig
