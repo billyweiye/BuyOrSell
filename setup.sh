13667f0b383c46f58ad07f5ad6f9eae4d302cc74
billyweiye <wyeah1980@gmail.com>
2024-11-04 15:13:38 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Download and install ta-lib C library
+mkdir -p /tmp/ta-lib
+cd /tmp/ta-lib
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib
+./configure --prefix=/usr
+make
+sudo make install
