a8891cc5b438c81da2af84e361d24d809231cf37
billyweiye <wyeah1980@gmail.com>
2024-11-04 23:32:23 +0800

    Update trend_analysis.py

--- trend_analysis.py
+++ trend_analysis.py
@@ -6,7 +6,7 @@ import pandas as pd
 def analyze_trend(data):
     data = calculate_trend_indicators(data) 
 
-    trend_signals = ['ma200_ma50_signal','ma_slope_signal','ema_dif_dea_signal','rsi14_signal','boll_width_signal','trend_line_signal','volume_price_signal']
+    trend_signals = ['ma100_ma20_signal','ma_slope_signal','ema_dif_dea_signal','rsi14_signal','boll_width_signal','trend_line_signal','volume_price_signal']
 
     # 计算趋势信号的累计和
     data['trend_signal'] = data[trend_signals].sum(axis=1)
